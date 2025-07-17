aad209142353882e2ddb15498f5d7f8eebe09c55
Lantea-Git <184443362+Lantea-Git@users.noreply.github.com>
2025-07-17 21:01:58 +0200

    inject bloc plutôt / cloudflare
    inject bloc plutôt 
    Le souci c'est qu'à moyen de faire un système de Local Storage comme les mosaïques là j'ai pas mis de garde fous mais de faire la requête à chaque fois avec le nouveau JVC justement en général 403 24 h après.

--- newfag_detector.user.js
+++ newfag_detector.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Newfag detecor
-// @version      2.4
+// @version      2.5
 // @description  Affiche l'ancienneté des pseudos qui le cachent
 // @author       NocturneX
 // @match        *://www.jeuxvideo.com/profil/*?mode=infos
@@ -36,21 +36,26 @@
 
     pseudoId = parseInt(pseudoId, 10);
 
-    let bloc;
+    let bloc = document.createElement('div');
+    bloc.classList.add('bloc-default-profil');
+    bloc.innerHTML = `
+      <div class="bloc-default-profil-header">
+        <h2>Newfag Detector</h2>
+      </div>
+      <div class="bloc-default-profil-body">
+        <ul class="display-line-lib">
+          <li>
+            <div class="info-lib">Membre depuis :</div>
+            <div class="info-value">
+            </div>
+          </li>
+        </ul>
+      </div>`;
+    col.insertBefore(bloc, col.children[1] || null);
+
+    // Injecte le contenu html
     const createBloc = (html) => {
-      if (!bloc) {
-        bloc = document.createElement('div');
-        bloc.classList.add('bloc-default-profil');
-        bloc.innerHTML = `
-          <div class="bloc-default-profil-header">
-            <h2>Newfag Detector</h2>
-          </div>
-          <div class="bloc-default-profil-body">
-          </div>`;
-        col.appendChild(bloc);
-      }
-      // Injecte le contenu html à chaque appel
-      bloc.querySelector('.bloc-default-profil-body').innerHTML = html;
+      bloc.querySelector('.info-value').innerHTML = html;
     };
 
     createBloc(`<p style="margin-left: 10px;">Chargement ...</p>`);
